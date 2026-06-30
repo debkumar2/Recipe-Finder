c688742446a7a66c36e0a4016816d7eaed1d435d
debkumar2 <debkumarmondal111@gmail.com>
2026-06-30 19:25:58 +0530

    Navbar color changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import brandImg from "../assets/brandLogo.png";
 import CmnButton from "./CmnButton";
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 w-full py-6  bg-black text-white px-14 flex items-center justify-between">
+    <div className="fixed top-0 left-0 w-full py-6  bg-white text-black px-14 flex items-center justify-between">
       <div className="w-[180px]">
         <img src={brandImg} alt="" className="w-full ojbect" />
       </div>
-      <ul className="flex">
+      <ul className="flex gap-8">
         <li className="px-4 py-2">
           <a href="" className="font-medium text-base">Home</a>
         </li>
